7686ea197ac4dd525286d4a8da05982986f87483
Enguix <77503498+Enguix@users.noreply.github.com>
2021-03-05 18:32:08 +0100

    examen

---
+++ ExamenDesarrolloPau4/javas/java.js
@@ -0,0 +1,13 @@
+document.getElementById('bola1').addEventListener('click',bol1);
+function bol1(){
+document.getElementById('imagen').style.backgroundImage="url('img/orcaero/ORCA_AREO_M21eTEAM.jpg')";
+}
+document.getElementById('bola2').addEventListener('click',bol2);
+function bol2(){
+document.getElementById('imagen').style.backgroundImage="url('img/orcaero/ORCA_AREO_M21eTEAMA.jpg')";
+}
+document.getElementById('bola3').addEventListener('click',bol3);
+function bol3(){
+	document.getElementById('imagen').style.backgroundImage="url('img/orcaero/ORCA_AREO_M21eTEAMB.jpg')";
+}
+
